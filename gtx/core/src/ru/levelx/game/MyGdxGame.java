85bb04cae4fab5f51b2809efaf21f30acd247d57
levelx <levelx@mail.ru>
2017-06-25 19:27:06 +0400

    ведро ездит по горизонтали

--- gtx/core/src/ru/levelx/game/MyGdxGame.java
+++ gtx/core/src/ru/levelx/game/MyGdxGame.java
@@ -2,32 +2,89 @@ package ru.levelx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.particles.values.RectangleSpawnShapeValue;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class MyGdxGame extends ApplicationAdapter {
+	OrthographicCamera camera;
 	SpriteBatch batch;
-	Texture img;
+	Texture dropImage;
+	Texture bucketImage;
+	Sound dropSound;
+	Music rainMusic;
+	Rectangle bucket;
+	Vector3 touchPos;
+
 	
 	@Override
 	public void create () {
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,800,400);
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		touchPos = new Vector3();
+
+		dropImage = new Texture("droplet.png");
+		bucketImage = new Texture("bucket.png");
+
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("waterdrop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("undertreeinrain.mp3"));
+
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+
+
+		if(Gdx.input.isTouched()){
+
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(),0);
+			camera.unproject(touchPos);
+			bucket.x = (int) (touchPos.x -64 / 2);
+		}
+
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
 	}
 }
