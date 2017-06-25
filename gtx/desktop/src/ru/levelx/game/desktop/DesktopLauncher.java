816c17f72d1ba01c2c4598feb36b6160ef161c4a
levelx <levelx@mail.ru>
2017-06-25 18:04:47 +0400

    установлено разрешение экрана 800 на 480

--- gtx/desktop/src/ru/levelx/game/desktop/DesktopLauncher.java
+++ gtx/desktop/src/ru/levelx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import ru.levelx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "vedrop";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
